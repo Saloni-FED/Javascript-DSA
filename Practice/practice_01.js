2bcf704aad0d5dc8a3f4c64310e7fea69aa448c1
Saloni-FED <salonipandey014@gmail.com>
2025-04-22 20:18:43 +0530

    javascript largestOddNumber

---
+++ Practice/practice_01.js
@@ -0,0 +1,10 @@
+var largestOddNumber = function(num) {
+    for(let i = num.length - 1 ; i >= 0 ; i-- ){
+        if(!Number(num[i] % 2 === 0)){
+            return num.slice(0,i+1);
+        }
+    }
+    return " "
+
+}
+console.log(largestOddNumber("35427"))
