7cfc13dd4911d51c5f98d69c2b92ba6483299f9b
Saloni-FED <salonipandey014@gmail.com>
2025-04-28 19:38:49 +0530

    Intevriew session questions

--- Interview-Question/Day-1.js
+++ Interview-Question/Day-1.js
@@ -1,3 +1,37 @@
 const num = [1,2,3];
 const abc = num.reduce((item)=> item = 2)
 console.log(abc)
+
+// Explain map , reduce , filter 
+// Explain what is json
+// Explain async await
+// Features of ES6
+// What is temporal dead zone
+// What is lexical scoping
+// Type of nan
+// local storage and session storage
+
+var x = 10 ;
+console.log(x++);
+console.log(x)
+console.log(typeof(NaN))
+
+
+// Function that returns longest word in an array
+let str = "salonii pandeyyy hai mera naam"
+function longWord (x){
+   let max = -1;
+   let ch = x.split(' ');
+   let word = '';
+   console.log(ch)
+   for(let i = 0 ; i < ch.length ; i++){
+    if(ch[i].length > max){
+        max = ch[i].length;
+        word = ch[i];
+    }
+
+   }
+return word;
+}
+
+console.log(longWord(str))
