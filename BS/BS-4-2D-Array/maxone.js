1a0226ae791087abe9c06a877577d3a10b9e6447
Saloni-FED <salonipandey014@gmail.com>
2025-05-08 21:06:40 +0530

    Solved Opti Max one BS probl

--- BS/BS-4-2D-Array/maxone.js
+++ BS/BS-4-2D-Array/maxone.js
@@ -23,3 +23,39 @@
 // }
 
 // console.log(row,"Row is here")
+
+
+// Optimal Approcah using Binary Search
+function lowerBound(arr, key){
+    let low = 0;
+    let high = arr.length;
+    let ans = -1;
+    while(low <= high){
+        let mid = Math.floor(low+high)/2;
+        if(arr[mid] >= key){
+            ans = mid ;
+            high = mid -1
+        }
+        else{
+            low = mid + 1
+        }
+    }
+return ans;
+}
+let n = 3
+let m = 3 
+let mat = [[1,1,1], [0,0,1],[0,0,0]]
+
+let max = 0;
+let row ;
+for(let i = 0 ; i < m; i++){
+    let cnt = m - lowerBound(mat[i],1 )
+    
+    if(cnt > max){
+        max = cnt;
+        row = i;
+    }
+
+}
+
+console.log(row)
