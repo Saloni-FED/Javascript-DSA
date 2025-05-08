854145d92112cf1940b5d2a115e0b14630c949c7
Saloni-FED <salonipandey014@gmail.com>
2025-05-08 20:33:36 +0530

    Solved BS 2d array problem

---
+++ BS/BS-4-2D-Array/maxone.js
@@ -0,0 +1,25 @@
+// My Brute force approach
+// let n = 3
+// let m = 3 
+// let mat = [[1,1,1], [0,0,1],[0,0,0]]
+
+// let max = 0;
+// let row ;
+// for(let i = 0 ; i < m; i++){
+//     let cnt = 0;
+//     for(let j = 0 ; j < m ;j++){
+//         if(mat[i][j] === 1
+
+//         )
+//         {
+//             cnt ++
+//         }
+//     }
+//     if(cnt > max){
+//         max = cnt;
+//         row = i;
+//     }
+
+// }
+
+// console.log(row,"Row is here")
