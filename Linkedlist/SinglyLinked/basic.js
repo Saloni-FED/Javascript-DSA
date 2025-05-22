c6e4345030a2784221e6a443ee677a37dd71b3b2
Saloni-FED <salonipandey014@gmail.com>
2025-05-22 22:20:32 +0530

    Linkedlist deletion head and tail

--- Linkedlist/SinglyLinked/basic.js
+++ Linkedlist/SinglyLinked/basic.js
@@ -5,13 +5,14 @@ class Node {
   }
 }
 
+// Insert the Node 
 function insertHead(head, value) {
 //   console.log(head, "head is here for insert head");
   let temp = new Node(value, head);
   return temp;
 }
 
-
+// Traverse The linkedlist
 function traverse (head){
     let temp = head ;
     while(temp !== null){
@@ -22,7 +23,7 @@ function traverse (head){
 }
 
 
-
+// Search in Linkedlist
 function search (value, head){
     let temp = head;
     while(temp !== null){
@@ -33,16 +34,42 @@ function search (value, head){
     }
     return false;
 }
+
+
+
+// Function for remove head
+function remove(head){
+  if(head === null) return head;
+  return head.next
+}
+
+
+// Function for remove tail
+function tail (head){
+  let temp = head;
+  while(temp.next.next !== null){
+    temp = temp.next
+  }
+temp.next = null;
+return head;
+}
+
+
+// 
 let node1 = new Node(10);
 node1.next = new Node(11);
 node1.next.next = new Node(20);
 // console.log(node1,"node1 is here")
 
+// Inserting head
 let insert = insertHead(node1, 100);
 // console.log(insert, "inserting the value");
 
 // console.log(traverse(node1))
 
-console.log(search(10, insert))
 
+// Remove
+
+// console.log(search(10, insert))
+console.log(remove(insert))
 
