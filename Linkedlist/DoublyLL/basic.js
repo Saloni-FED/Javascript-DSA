bb44c32d8e3cfda8bd8a8c555e8791afffed3afb
Saloni-FED <salonipandey014@gmail.com>
2025-05-28 20:07:07 +0530

    solve delete kth element from DoublyLL

--- Linkedlist/DoublyLL/basic.js
+++ Linkedlist/DoublyLL/basic.js
@@ -26,32 +26,58 @@ class Node {
       );
     }
   }
-  static insertionHead(head , val){
-    let newHead = new Node (val , head )
-    if(head !== null){
-        head.prev = newHead
+  static insertionHead(head, val) {
+    let newHead = new Node(val, head);
+    if (head !== null) {
+      head.prev = newHead;
     }
     return newHead;
   }
   // Delete the head
-  static deleteHead(head){
-    if(head === null || head.next === null) return null
+  static deleteHead(head) {
+    if (head === null || head.next === null) return null;
     let newPrev = head;
     head = newPrev.next;
     head.prev = null;
     newPrev.next = null;
-    return head
+    return head;
   }
-  static deleteTail(head){
-     if(head === null || head.next === null) return null
-     let tail = head;
-     while(tail.next  !== null){
+  // Delete the node
+  static deleteTail(head) {
+    if (head === null || head.next === null) return null;
+    let tail = head;
+    while (tail.next !== null) {
       tail = tail.next;
-     }
-     let newTail = tail.prev;
-     tail.prev = null;
-     newTail.next = null;
-     return head;
+    }
+    let newTail = tail.prev;
+    tail.prev = null;
+    newTail.next = null;
+    return head;
+  }
+
+  // Delete the K th element from DLL
+  static deleteKEl(head, k) {
+    let cnt = 0;
+    let temp = head;
+    while (temp !== null) {
+      cnt++;
+      if (cnt === k) break;
+      temp = temp.next;
+    }
+    let prevEl = temp.prev;
+    let nextEl = temp.next;
+    if (prevEl === null && nextEl === null) return null;
+    else if (prevEl === null) {
+      Node.deleteHead(head);
+    } else if (nextEl === null) {
+      Node.deleteTail(head);
+    } else {
+      prevEl.next = nextEl;
+      nextEl.prev = prevEl;
+      temp.next = null;
+      temp.prev = null;
+    }
+    return head;
   }
 }
 
@@ -59,18 +85,18 @@ let arr = [12, 1, 3, 8];
 let nodeList = Node.covertArr2DLL(arr);
 // console.log(nodeList,"nodeList is here")
 
-
-
 // Traversing the data
 // Node.traverseDD(nodeList);
 
-
-
 // Insertion in head;
-let newNodeHead = Node.insertionHead(nodeList,5)
+let newNodeHead = Node.insertionHead(nodeList, 5);
 
 // Delete head
-let deletedHead = Node.deleteHead(newNodeHead)
+let deletedHead = Node.deleteHead(newNodeHead);
 // Delete Tail
-let deleteTail = Node.deleteTail(deletedHead)
-Node.traverseDD(deleteTail);
+let deleteTail = Node.deleteTail(deletedHead);
+// Node.traverseDD(deleteTail);
+
+// Delete Kth element 
+let deleteKthElement = Node.deleteKEl(deleteTail,2)
+Node.traverseDD(deleteKthElement )
